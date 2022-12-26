f9307a1b670e8f9b1bda7b48ceb446d67c334e05
Ubuntu <visuwesh@visuwesh.4v1u1eilp2ouroqowbboayq0ja.rx.internal.cloudapp.net>
2022-12-26 08:52:07 +0000

    registry changes

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 }
 resource "azurerm_container_registry" "acr" {
-  name     = "myrepo"
+  name     = "myrepovisuwesh1211"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   sku                      = "Basic"
