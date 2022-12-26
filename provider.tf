2a738402a0061a5a9d8d738a4a87da98c8c1e1dd
Ubuntu <visuwesh@visuwesh.4v1u1eilp2ouroqowbboayq0ja.rx.internal.cloudapp.net>
2022-12-26 03:53:46 +0000

    secret changed

--- provider.tf
+++ provider.tf
@@ -9,9 +9,9 @@ terraform {
 
 provider "azurerm" {
    features {}
-subscription_id   = 6695e146-bcf7-4318-9381-b93118cb45d1
-  tenant_id       = 9509f3ca-f0d2-43b6-9340-913e7e8d0829
-  client_id       = e5fd2d48-a242-4b8d-97b0-3880f57c7476
-  client_secret   = 6G18Q!YHOS86Kp9ruu95DJwvyHKUBiOxU61sUb4z
+subscription_id   = "6695e146-bcf7-4318-9381-b93118cb45d1"
+  tenant_id       = "9509f3ca-f0d2-43b6-9340-913e7e8d0829"
+  client_id       = "e5fd2d48-a242-4b8d-97b0-3880f57c7476"
+  client_secret   = "6G18Q~YHOS86Kp9ruu95DJwvyHKUBiOxU61sUb4z"
 }
 
